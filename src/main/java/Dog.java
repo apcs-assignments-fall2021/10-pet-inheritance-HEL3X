31d9d3a40a4bffce66f123500a3f0d9c89a83d3c
HEL3X <person_of_paradise@hotmail.com>
2021-10-31 20:41:14 -0700

    Finished

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,70 @@
 public class Dog extends Pet {
     // Instance variable(s)
+    private String name;
+    private int age;
+    private String breed;
 
 
     // Constructors
-    
+    public Dog(String name, int age, String breed){
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+    public Dog(){
+        this.name = "Walter";
+        this.age = 10;
+        this.breed = "Bull Terrier";
+    }
 
     // makeNoise() method
 
+    @Override
+    public void makeNoise() {
+        System.out.println("Bark! Bark! Bark!");
+    }
 
     // toString method
+
+    @Override
+    public String toString() {
+        return "Dog: " + "\n" +
+                "   Name: " + name + "\n" +
+                "   Age: " + age + "\n" +
+                "   Breed: " + breed;
+    }
 
 
     // Getter
 
+    public String getBreed() {
+        return breed;
+    }
 
-    // Setter
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+
+    //Setter
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,11 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog d1 = new Dog();
+        d1.setBreed("English Greyhound");
+        d1.setAge(3);
+        d1.setName("Doggy");
+        System.out.println(d1.toString());
+        System.out.println();
+        d1.makeNoise();
     }
 }
